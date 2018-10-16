cb220f8dfb0b39923f300c0e8b0c7f81b5f07d58
Nathan Coit <coitn@mcmaster.ca>
2018-10-16 16:42:16 -0400

    Initial unity project commit

---
+++ Assets/Scripts/ManagementMode/GameManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Game manager for the Management mode. Controls intializing of level 
+/// and transitions from Combat Mode to and from Management Mode.
+/// </summary>
+public class GameManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}

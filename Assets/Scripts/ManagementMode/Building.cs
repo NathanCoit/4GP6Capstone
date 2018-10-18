ab37637d8227dbc6c0c39d45172115d5e142754c
Nathan Coit <coitn@mcmaster.ca>
2018-10-18 16:25:51 -0400

    starting framework for map and buildings

---
+++ Assets/Scripts/ManagementMode/Building.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/// <summary>
+/// Class containing methods for creating, interacting with,and deleting building objects
+/// </summary>
+public class Building : MonoBehaviour {
+    private GameObject mgobjBuilding;
+    public Vector2 BuildingPosition;
+
+    public enum BUILDING_TYPE
+    {
+        MATERIAL,
+        HOUSING,
+        VILLAGE,
+        ALTAR
+    }
+
+    public BUILDING_TYPE BuildingType { get; private set; }
+
+	public Building(BUILDING_TYPE penumBuildingType)
+    {
+        mgobjBuilding = CreateBuildingObject(penumBuildingType);
+        BuildingType = penumBuildingType;
+    }
+
+
+    private GameObject CreateBuildingObject(BUILDING_TYPE penumBuildingType)
+    {
+        GameObject gobjBuilding = null;
+        switch (penumBuildingType)
+        {
+            case BUILDING_TYPE.ALTAR:
+                gobjBuilding = CreateAltarBuildingObject();
+                break;
+            case BUILDING_TYPE.VILLAGE:
+                gobjBuilding = CreateVillageBuildingObject();
+                break;
+        }
+        return gobjBuilding;
+    }
+
+    private GameObject CreateVillageBuildingObject()
+    {
+        GameObject gobjVillageBuilding = null;
+
+        return gobjVillageBuilding;
+    }
+    private GameObject CreateAltarBuildingObject()
+    {
+        GameObject gobjAltarBuilding = null;
+
+        return gobjAltarBuilding;
+    }
+    
+}
---
+++ Assets/Scripts/ManagementMode/TerrainMap.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// The terrain map object holding methods for interacting with the map object
+/// </summary>
+public class TerrainMap : MonoBehaviour {
+    private GameObject mgobjTerrainMap;
+    private List<Building> marrBuildingsOnMap;
+
+    public TerrainMap()
+    {
+        mgobjTerrainMap = CreateTerrainObject();
+        marrBuildingsOnMap = new List<Building>();
+    }
+	
+
+    private GameObject CreateTerrainObject()
+    {
+        GameObject gobjMap = null;
+        // Create map
+
+        return gobjMap;
+    }
+
+    public bool PlaceBuilding(Building pBuildingToPlace, Vector2 pvec2PointToPlace)
+    {
+        bool blnCanPlace = false;
+        marrBuildingsOnMap.Add(pBuildingToPlace);
+        //Attempt to place building and return result
+
+        if(blnCanPlace)
+        {
+            pBuildingToPlace.BuildingPosition = pvec2PointToPlace;
+        }
+        return blnCanPlace;
+    }
+
+    public List<Building> GetBuildings()
+    {
+        return marrBuildingsOnMap;
+    }
+}

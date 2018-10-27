4a2523e4fbbe047bd958e5c64a33260f6a02f2da
Elijah Wilding <elijahwilding@gmail.com>
2018-10-26 20:44:00 -0400

    simple UI added

---
+++ Assets/Scripts/ManagementMode/modeTextScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class modeTextScript : MonoBehaviour {
+
+	Text modeText;
+	// Use this for initialization
+	void Start () {
+		modeText = gameObject.GetComponent<Text> ();
+		modeText.text = "Explore Mode";
+	}
+
+	// Update is called once per frame
+	void Update () {
+	}
+
+	public void textChange(string text){
+		switch (text) 
+		{
+		case "explore":
+			modeText.text = "Explore Mode";
+			break;
+		case "building":
+			modeText.text = "Building Mode";
+			break;
+		}
+	}
+}

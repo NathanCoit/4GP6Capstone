7527a56afe126101685cb82cb7dc5c95704221b8
Nathan Coit <coitn@mcmaster.ca>
2018-11-20 14:30:09 -0500

    gave up again again

--- build.sh
+++ build.sh
@@ -9,9 +9,6 @@ echo "Running play unit tests for UnityProject"
 /Applications/Unity/Unity.app/Contents/MacOS/Unity -batchmode -testPlatform playmode -projectPath "$(pwd)/UnityProject" -runTests -testResults "$(pwd)/test.xml"
 
 rc0=$?
-echo "Unit test logs"
-if [-f $(pwd)/test.xml ]; then
-	cat ./UnityProject/test.xml
 # exit if tests failed
 if [ $rc0 -ne 0 ]; then { echo "Failed unit tests"; exit $rc0; } fi
 

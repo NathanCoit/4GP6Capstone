ce2de775327688632c982bb2c747625700c362bd
Nathan Coit <coitn@mcmaster.ca>
2018-11-20 13:19:02 -0500

    gave up

--- build.sh
+++ build.sh
@@ -4,16 +4,14 @@
 #  a subdirectory of the repo root directory, e.g. for this repo "unity-ci-test" 
 #  the project folder is "UnityProject". If this is not true then adjust the 
 #  -projectPath argument to point to the right location.
-ls
 ## Run the play unit tests
 echo "Running play unit tests for UnityProject"
 /Applications/Unity/Unity.app/Contents/MacOS/Unity -batchmode -testPlatform playmode -projectPath "$(pwd)/UnityProject" -runTests -testResults "$(pwd)/test.xml"
 
 rc0=$?
-ls $(pwd)
-ls ./
 echo "Unit test logs"
-cat ./UnityProject/test.xml
+if [-f $(pwd)/test.xml ]; then
+	cat ./UnityProject/test.xml
 # exit if tests failed
 if [ $rc0 -ne 0 ]; then { echo "Failed unit tests"; exit $rc0; } fi
 
